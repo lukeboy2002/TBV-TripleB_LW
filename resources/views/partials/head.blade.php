be96c8d92b985842d9be12676a875afc8836dfe5
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2025-04-18 14:52:22 +0200

    Flowbite
    added flowbite

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,10 +1,12 @@
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 <title>{{ $title ?? 'Laravel' }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
+<link href="https://fonts.bunny.net/css?family=montserrat:100,200,300,400,500,600,700,800,900&display=swap"
+      rel="stylesheet"/>
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
